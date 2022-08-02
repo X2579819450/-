ad691d6eed16f5950233e47eec3d07be50e9ff37
谢金鹏 <itheima@itcast>
2022-08-02 18:16:55 +0800

    index分支

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,4 @@
 const baseURL = "http://www.liulongbin.top:3007"
-$.ajaxPrefilter(Option => {
-  Option.url = baseURL + option.url
+$.ajaxPrefilter(option => {
+  option.url = baseURL + option.url
 })
